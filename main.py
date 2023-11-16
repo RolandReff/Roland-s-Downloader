5ebbd81f5ab406ba1b3e10a7348ab267cd7de380
RolandReff <Henrik.reffs@gmail.com>
2023-11-16 04:04:23 +0000

    Removed config, updated main, completed test_main

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import pytube
 import youtube_dl
 import ffmpeg
 
-
-from config import config
-
-
 # 18/8/2023
 #TLDR, Google enginers are at warfare with the devs, use this ciper for the pytube downnload function to work with 15.0.0
 # https://github.com/oncename/pytube/blob/master/pytube/cipher.py
@@ -24,19 +20,6 @@ def is_supported(URL): #https://stackoverflow.com/a/61489622
     except:
         return False
 
-def Menu(): 
-    global mainConfig 
-    mainConfig = config()
-    mainConfig.createDownloadFolder()
-    
-    APPSTATE = True
-    while APPSTATE == True: 
-        print('URL example: https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-        URL = input("Enter URL: ")
-        print('Options:\n1-MP4\n2-MP3') #\n3-WAV not added yet to both downloaders
-        Mode = input("Enter format:")
-        DownloaderChoice(URL, Mode)
-
 def TestMenu(): #Reserved funtion for testing
     print("Hei Verden")
     
@@ -51,12 +34,12 @@ def DownloaderChoice(URL, Mode):
 def YoutubeDownloader(URL, Mode):    
     yt = pytube.YouTube(URL)
     if Mode == "mp4" or Mode == "MP4" or Mode == "Mp4" or Mode == str(1):
-        input1 = yt.streams.get_by_itag(ItagChecker(yt,False)).download(output_path=mainConfig.downloaldFolderPath)
+        input1 = yt.streams.get_by_itag(ItagChecker(yt,False)).download(output_path=".\download")
         base1, ext1 = os.path.splitext(input1)
-        os.rename(input1,os.path.join(mainConfig.downloaldFolderPath,'input-a'+ext1))
-        input2 = yt.streams.get_by_itag(ItagChecker(yt,True)).download(output_path=mainConfig.downloaldFolderPath)
+        os.rename(input1,os.path.join(".\download",'input-a'+ext1))
+        input2 = yt.streams.get_by_itag(ItagChecker(yt,True)).download(output_path=".\download")
         base2, ext2 = os.path.splitext(input2)
-        os.rename(input2,os.path.join(mainConfig.downloaldFolderPath,'input-b'+ext2))
+        os.rename(input2,os.path.join(".\download",'input-b'+ext2))
 
         input_video = ffmpeg.input(r'./download/input-a'+ext1)
         input_audio = ffmpeg.input(r'./download/input-b'+ext2)
@@ -64,22 +47,22 @@ def YoutubeDownloader(URL, Mode):
         stream = ffmpeg.output(input_video, input_audio, './download/output.mp4',vcodec = 'copy', crf = 'copy', acodec = 'copy')
         ffmpeg.run(stream)
 
-        os.rename(os.path.join(mainConfig.downloaldFolderPath,"output.mp4"),os.path.join(mainConfig.downloaldFolderPath,base1+".mp4"))
+        os.rename(os.path.join(".\download","output.mp4"),os.path.join(".\download",base1+".mp4"))
 
-        os.remove(os.path.join(mainConfig.downloaldFolderPath,"input-a"+ext1))
-        os.remove(os.path.join(mainConfig.downloaldFolderPath,"input-b"+ext2))
+        os.remove(os.path.join(".\download","input-a"+ext1))
+        os.remove(os.path.join(".\download","input-b"+ext2))
     
         print('"'+yt.title+'"'+ " has been downloaded")
 
     elif Mode == "mp3" or Mode == "MP3" or Mode == "Mp3" or Mode == str(2): #Downloads only audio as a MP4 file, need conversion to MP3
-        input = yt.streams.get_by_itag(ItagChecker(yt,True)).download(output_path=mainConfig.downloaldFolderPath)
+        input = yt.streams.get_by_itag(ItagChecker(yt,True)).download(output_path=".\download")
         base, ext = os.path.splitext(input)
-        os.rename(input,os.path.join(mainConfig.downloaldFolderPath,'input'+ext))
+        os.rename(input,os.path.join(".\download",'input'+ext))
         input_audio = ffmpeg.input(r'./download/input'+ext)
         stream = ffmpeg.output(input_audio, './download/output.mp3',crf = 'copy', acodec = 'libmp3lame', format = 'mp3')
         ffmpeg.run(stream)
-        os.rename(os.path.join(mainConfig.downloaldFolderPath,"output.mp3"),os.path.join(mainConfig.downloaldFolderPath,base+".mp3"))
-        os.remove(os.path.join(mainConfig.downloaldFolderPath,"input"+ext))
+        os.rename(os.path.join(".\download","output.mp3"),os.path.join(".\download",base+".mp3"))
+        os.remove(os.path.join(".\download","input"+ext))
         print(yt.title +" has been downloaded") 
     else:
         print("There has been a problem")
@@ -103,11 +86,11 @@ def ItagChecker(yt,audioTrueOrFalse): #Need to swap around the lists so i search
 
 def OtherDownloader(URL, Mode):
     if Mode == "mp4" or Mode == "MP4" or Mode == "Mp4" or Mode == str(1):
-        ydl_opts = {format: 'bestvideo+bestaudi','outtmpl': os.path.join(mainConfig.downloaldFolderPath,'%(title)s.%(ext)s'), 'ffmpeg-location': mainConfig.ffmpegPath,'ffprobe-loocation': mainConfig.ffprobePath,'postprocessors': [{ 'key': 'FFmpegVideoConvertor','preferedformat': 'mp4', }],} #Format is quality of download, outtmpl is where downloaded video end up 
+        ydl_opts = {format: 'bestvideo+bestaudi','outtmpl': os.path.join("\download",'%(title)s.%(ext)s'),'postprocessors': [{ 'key': 'FFmpegVideoConvertor','preferedformat': 'mp4', }],} #Format is quality of download, outtmpl is where downloaded video end up 
     elif Mode == "mp3" or Mode == "MP3" or Mode == "Mp3" or Mode == str(2):
-        ydl_opts = {'format': 'bestaudio/best','outtmpl': os.path.join(mainConfig.downloaldFolderPath,'%(title)s.%(ext)s'), 'ffmpeg-location': mainConfig.ffmpegPath,'ffprobe-loocation': mainConfig.ffprobePath ,'postprocessors': [{'key': 'FFmpegExtractAudio','preferredcodec': 'mp3', 'preferredquality': '192',}],} #Format is quality of download, outtmpl is where downloaded video end up
+        ydl_opts = {'format': 'bestaudio/best','outtmpl': os.path.join("\download",'%(title)s.%(ext)s'),'postprocessors': [{'key': 'FFmpegExtractAudio','preferredcodec': 'mp3', 'preferredquality': '192',}],} #Format is quality of download, outtmpl is where downloaded video end up
     elif Mode == "wav" or Mode == "WAV" or Mode == "Wav" or Mode == str(3):
-        ydl_opts = {'format': 'bestaudio/best','outtmpl': os.path.join(mainConfig.downloaldFolderPath,'%(title)s.%(ext)s'), 'ffmpeg-location': mainConfig.ffmpegPath,'ffprobe-loocation':mainConfig.ffprobePath,'postprocessors': [{'key': 'FFmpegExtractAudio','preferredcodec': 'wav',}],} #WAV template for later use
+        ydl_opts = {'format': 'bestaudio/best','outtmpl': os.path.join("\download",'%(title)s.%(ext)s'),'postprocessors': [{'key': 'FFmpegExtractAudio','preferredcodec': 'wav',}],} #WAV template for later use
 
     else:
         print("There has been a problem")
@@ -118,10 +101,16 @@ def OtherDownloader(URL, Mode):
         print(str('"'+download.get('title', None)) + '" has been downloaded')
 
 if __name__ == "__main__":
-    Menu()
+    if not os.path.isdir("download"): # Makes a download path if there isn't one
+            os.makedirs("download")
+            print("download folder created!")
     
-    
-
-
+    APPSTATE = True
+    while APPSTATE == True: 
+        print('URL example: https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+        URL = input("Enter URL: ")
+        print('Options:\n1-MP4\n2-MP3') #\n3-WAV not added yet to both downloaders
+        Mode = input("Enter format:")
+        DownloaderChoice(URL, Mode)
     
     
--- test_main.py
+++ test_main.py
@@ -1,16 +1,33 @@
 import unittest
 import subprocess
 import main
+import os
 
 class test_main(unittest.TestCase):
     def test_yt_dl_installed(self):
-        self.assertEqual(subprocess.check_call("where youtube-dl"),0)
+        self.assertNotEqual(subprocess.check_output("where youtube-dl"),"INFO: Could not find files for the given pattern(s).")
 
     def test_ffprobe_installed(self):
-        self.assertEqual(subprocess.check_call("where ffprobe"),0)
+        self.assertNotEqual(subprocess.check_output("where ffprobe"),"INFO: Could not find files for the given pattern(s).")
 
     def test_ffmpeg_installed(self):
-        self.assertEqual(subprocess.check_call("where ffmpeg"),0)
+        self.assertNotEqual(subprocess.check_output("where ffmpeg"),"INFO: Could not find files for the given pattern(s).")
 
+    def test_files_youtube_downloaded(self):
+        main.YoutubeDownloader("https://www.youtube.com/watch?v=vGyHXW0lwZY","mp4")
+        self.assertTrue(os.path.isfile(r".\download\Doggo Takes Flight.mp4"))
     
+        main.YoutubeDownloader("https://www.youtube.com/watch?v=zdeZwAk6ULE","mp3")
+        self.assertTrue(os.path.isfile(r".\download\Undertale OST 024 - Bonetrousle.mp3"))
+
+    def test_files_other_downloader(self):
+        main.OtherDownloader("https://tv.nrk.no/serie/foerstegangstjenesten/ekstramateriale/KMNO10007421/avspiller","mp4")
+        self.assertTrue(os.path.isfile(r".\download\Førstegangstjenesten.mp4"))
+        
+        main.OtherDownloader("https://soundcloud.com/rick-astley-official/never-gonna-give-you-up-4","mp3")
+        self.assertTrue(os.path.isfile(r".\download\Never Gonna Give You Up.mp3"))
+
+if __name__ == '__main__':
+    unittest.main()
+
     
