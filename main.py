9cdb71ef18da8236e3e31ab83a9b8c1a8aab4704
RolandReff <Henrik.reffs@gmail.com>
2024-08-17 01:31:18 +0200

    Progress bar now is able to be updated by downloads from pytubefix Still work to be done for bar to reflect actual progress

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
 import os
 import re #Add regex later in Pytubedownloader
-import pytubefix 
+import pytubefix
+from pytubefix.cli import on_progress 
 import yt_dlp 
 import ffmpeg
 import sys
@@ -20,17 +21,25 @@ def is_supported(url):
             return True
     return False
     
-def DownloaderChoice(URL, Mode):
+def DownloaderChoice(_self,URL, Mode):
     if ("www.youtube.com" in URL):
-        PytubeDowloader(URL, Mode)
+        PytubeDowloader(_self,URL, Mode)
         #ytdlpDownloader(URL, Mode)
     elif (is_supported(URL)):
-        ytdlpDownloader(URL, Mode)
+        ytdlpDownloader(_self,URL, Mode)
     else:   
        print("The URL is not valid or not supported.")
 
-def PytubeDowloader(URL, Mode):    
+def downloadPercentage(stream,chunk,bytes_remaining):
+    
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_of_completion = bytes_downloaded / total_size * 100
+    window.ProgressBar.setValue(round(percentage_of_completion))
+
+def PytubeDowloader(_self,URL, Mode):    
     yt = pytubefix.YouTube(URL)
+    yt.register_on_progress_callback(downloadPercentage)
     if Mode == "mp4" or Mode == "MP4" or Mode == "Mp4" or Mode == str(1):
         input1 = yt.streams.get_by_itag(ItagChecker(yt,"video")).download(output_path=".\download")
         FileName, ext1 = os.path.splitext(input1)
@@ -86,9 +95,9 @@ def ItagChecker(yt,audioOrVideo): #TO-DO: Make it so older videos where audio an
     return itag
 
 
-def ytdlpDownloader(URL, Mode):
+def ytdlpDownloader(self,URL, Mode):
     if Mode == "mp4" or Mode == "MP4" or Mode == "Mp4" or Mode == str(1):
-        ydl_opts = {format: 'bestvideo+bestaudi','outtmpl': os.path.join("\download",'%(title)s.%(ext)s'),'postprocessors': [{ 'key': 'FFmpegVideoConvertor','preferedformat': 'mp4', }],} #Format is quality of download, outtmpl is where downloaded video end up 
+        ydl_opts = {format: 'bestvideo+bestaudi','outtmpl': os.path.join("\download",'%(title)s.%(ext)s'),'progress-template':print(),'postprocessors': [{ 'key': 'FFmpegVideoConvertor','preferedformat': 'mp4', }],} #Format is quality of download, outtmpl is where downloaded video end up 
     elif Mode == "mp3" or Mode == "MP3" or Mode == "Mp3" or Mode == str(2):
         ydl_opts = {'format': 'bestaudio/best','outtmpl': os.path.join("\download",'%(title)s.%(ext)s'),'postprocessors': [{'key': 'FFmpegExtractAudio','preferredcodec': 'mp3', 'preferredquality': '192',}],} #Format is quality of download, outtmpl is where downloaded video end up
     elif Mode == "wav" or Mode == "WAV" or Mode == "Wav" or Mode == str(3):
@@ -106,29 +115,33 @@ def ytdlpDownloader(URL, Mode):
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        
         self.setWindowTitle("Roland's Downloader")
         self.GridLayout = QGridLayout()
-        
+        #self.GridLayout.setContentsMargins(0,0,0,0)
         self.UrlInputField = QLineEdit()
-        self.GridLayout.addWidget(self.UrlInputField,0,0,1,4)
+        self.UrlInputField.setFixedWidth(450)
+        self.GridLayout.addWidget(self.UrlInputField,0,0,1,10)
         
         StartButton = QPushButton("Go")
         StartButton.clicked.connect(self.download)
-        self.GridLayout.addWidget(StartButton,0,5)
+        self.GridLayout.addWidget(StartButton,0,11)
         
         self.FileTypeDropDown = QComboBox(self)
-    
         self.FileTypeDropDown.addItem("Mp4")
         self.FileTypeDropDown.addItem("Mp3")
-
         
-           
         self.GridLayout.addWidget(self.FileTypeDropDown,1,0)
+        
+
         self.QualityDropDown = QComboBox(self)
         self.QualityDropDown.addItem("Best")
         self.QualityDropDown.addItem("Lowest")
-        self.GridLayout.addWidget(self.QualityDropDown,1,1)     
+        self.GridLayout.addWidget(self.QualityDropDown,1,1)   
+        ProgressBarPercantage = 0
+        self.ProgressBar = QProgressBar(self)
+        
+        self.GridLayout.addWidget(self.ProgressBar,2,0,1,10)
+        
 
         #self.GridLayout.setColumnStretch(0, 15)
         
@@ -142,7 +155,9 @@ class MainWindow(QMainWindow):
 
     def download(self):
         print(self.FileTypeDropDown.currentText())
-        DownloaderChoice(self.UrlInputField.text(),self.FileTypeDropDown.currentText())
+        self.ProgressBar.setValue(0)
+        DownloaderChoice(self,self.UrlInputField.text(),self.FileTypeDropDown.currentText())
+        
         
 
 
